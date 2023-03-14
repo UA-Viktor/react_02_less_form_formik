31ae249dbb37ca8a07fa37a5de78536179abdb5d
UA-Viktor <v.korenkov@icloud.com>
2023-03-14 20:07:10 +0200

    gogo

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+// import { ProductReviewForm } from './ProductReviewForm/ProductReviewForm';
+import { LoginForm } from './LoginForm/LoginForm';
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+        <LoginForm />
+        {/* <ProductReviewForm /> */}
+      </>
+    );
+  }
+}
